47f94fa3d7acc6103993a6fc3149ceadebf7c250
SubVray <jim.alberto.21@gmail.com>
2024-08-06 18:59:40 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { seoConfig } from '@/utils/seoConfig';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from "astro/config";
+import { seoConfig } from "./src/utils/seoConfig";
 
 // https://astro.build/config
 export default defineConfig({
