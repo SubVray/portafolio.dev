a17d5433c196a63a8ae667fd7538c98b71e08220
SubVray <jim.alberto.21@gmail.com>
2024-08-06 18:41:52 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
-import { defineConfig } from "astro/config"
-
 import tailwind from "@astrojs/tailwind"
+import vercelStatic from "@astrojs/vercel/static"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
+	build: {
+		inlineStylesheets: "always",
+	},
+	compressHTML: true,
+	prefetch: true,
 	devToolbar: {
 		enabled: false,
 	},
 	integrations: [tailwind()],
+	output: "server",
+	adapter: vercelStatic(),
 })
