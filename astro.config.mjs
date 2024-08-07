8d52c6faba831d8f1b03bc6d8c1255b25a819f7b
SubVray <jim.alberto.21@gmail.com>
2024-08-06 18:57:50 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import { seoConfig } from '@/utils/seoConfig';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from "astro/config";
@@ -15,5 +16,5 @@ export default defineConfig({
 	integrations: [tailwind()],
   output: 'server',
   adapter: vercel(),
-	site: 
+	site: seoConfig.baseURL
 })
